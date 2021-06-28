9c22daaf97121d44b484996853456c98b284ed4e
jaankuu <jaankuusaru@gmail.com>
2021-06-29 00:24:04 +0200

    created models for user and recipe

---
+++ models/user.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      user.hasMany(models.recipe)
+    }
+  };
+  user.init({
+    name: { type: DataTypes.STRING, allowNull: false, unique: true },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false},
+    profileUrl: { type: DataTypes.STRING }
+  }, {
+    sequelize,
+    modelName: 'user',
+  });
+  return user;
+};
