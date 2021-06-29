e6e4c66da2bd846ab8ea62636723213eb8986e57
jaankuu <jaankuusaru@gmail.com>
2021-06-29 12:00:21 +0200

    get recipes endpoint

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const { PORT } = require("./config/constants");
 const loggerMiddleWare = require("morgan");
 const corsMiddleWare = require("cors");
-// const authRouter = require("./routers/auth");
-// const authMiddleWare = require("./auth/middleware");
+const authRouter = require("./routers/auth");
+const authMiddleWare = require("./auth/middleware");
 const Users = require("./models").user;
 const Recipes = require("./models").recipe;
 
@@ -14,32 +14,34 @@ const bodyParserMiddleWare = express.json();
 app.use(bodyParserMiddleWare);
 app.use(corsMiddleWare());
 
+app.use("/", authRouter);
 
 app.get("/", (req, res) => {
     res.send("Hi from express");
   });
 
 
-  app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
   });
 
+
   // POST endpoint which requires a token for testing purposes, can be removed
-// app.post("/authorized_post_request", authMiddleWare, (req, res) => {
-//     // accessing user that was added to req by the auth middleware
-//     const user = req.user;
-//     // don't send back the password hash
-//     delete user.dataValues["password"];
+app.post("/authorized_post_request", authMiddleWare, (req, res) => {
+    // accessing user that was added to req by the auth middleware
+    const user = req.user;
+    // don't send back the password hash
+    delete user.dataValues["password"];
   
-//     res.json({
-//       youPosted: {
-//         ...req.body,
-//       },
-//       userFoundWithToken: {
-//         ...user.dataValues,
-//       },
-//     });
-//   });
+    res.json({
+      youPosted: {
+        ...req.body,
+      },
+      userFoundWithToken: {
+        ...user.dataValues,
+      },
+    });
+  });
 
 
 // get all recipes of a user
