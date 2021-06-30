0d12b495c04878767e4c2ad1d928466791fe5d0b
gc0rtes <guilhermeco@gmail.com>
2021-06-30 16:27:07 +0200

    add favorites recipes to /login /signup and /me

--- routers/auth.js
+++ routers/auth.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { toJWT } = require("../auth/jwt");
 const authMiddleware = require("../auth/middleware");
 const User = require("../models/").user;
+const Recipes = require("../models").recipe;
 const { SALT_ROUNDS } = require("../config/constants");
 
 const app = new express();
@@ -21,13 +22,19 @@ app.post("/login", async (req, res, next) => {
 
     if (!user || !bcrypt.compareSync(password, user.password)) {
       return res.status(400).send({
-        message: "User with that email not found or password incorrect"
+        message: "User with that email not found or password incorrect",
       });
     }
 
     delete user.dataValues["password"]; // don't send back the password hash
     const token = toJWT({ userId: user.id });
-    return res.status(200).send({ token, ...user.dataValues });
+
+    console.log("what is user.id", user.id);
+
+    const recipes = await Recipes.findAll({ where: { userId: user.id } });
+    console.log("what is recipes", recipes);
+
+    return res.status(200).send({ token, ...user.dataValues, recipes });
   } catch (error) {
     console.log(error);
     return res.status(400).send({ message: "Something went wrong, sorry" });
@@ -51,7 +58,7 @@ app.post("/signup", async (req, res) => {
 
     const token = toJWT({ userId: newUser.id });
 
-    res.status(201).json({ token, ...newUser.dataValues });
+    res.status(201).json({ token, ...newUser.dataValues, recipes: [] });
   } catch (error) {
     if (error.name === "SequelizeUniqueConstraintError") {
       return res
@@ -67,9 +74,10 @@ app.post("/signup", async (req, res) => {
 // - get the users email & name using only their token
 // - checking if a token is (still) valid
 app.get("/me", authMiddleware, async (req, res) => {
+  const recipes = await Recipes.findAll({ where: { userId: req.user.id } });
   // don't send back the password hash
   delete req.user.dataValues["password"];
-  res.status(200).send({ ...req.user.dataValues });
+  res.status(200).send({ ...req.user.dataValues, recipes });
 });
 
 module.exports = app;
